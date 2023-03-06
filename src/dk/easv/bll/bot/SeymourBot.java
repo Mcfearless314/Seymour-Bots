e33f7db0c83b6005b2714cd6ae17c63564759087
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-06 09:36:18 +0100

    simple random

--- src/dk/easv/bll/bot/SeymourBot.java
+++ src/dk/easv/bll/bot/SeymourBot.java
@@ -3,14 +3,31 @@ package dk.easv.bll.bot;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
+import java.util.List;
+import java.util.Random;
+
 public class SeymourBot implements IBot{
+    private static final String BOTNAME = "Seymour-Bot";
+    private Random rand = new Random();
+
     @Override
     public String getBotName() {
-        return "SeymourBot";
+        return BOTNAME;
     }
 
     @Override
     public IMove doMove(IGameState state) {
+        List<IMove> moves = state.getField().getAvailableMoves();
+        IMove move = random(moves);
+
+        return move;
+    }
+
+    private IMove random(List<IMove> moves) {
+        if (moves.size() > 0) {
+            return moves.get(rand.nextInt(moves.size()));
+        }
+
         return null;
     }
 }
