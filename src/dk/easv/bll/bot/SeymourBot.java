f1e0c5f59c96b57cde7555c0ca9af7ebeb8872a6
TheDarkSide1992 <Jenscp171@gmail.com>
2023-03-07 09:41:57 +0100

    SeymourBot 2.0

--- src/dk/easv/bll/bot/SeymourBot.java
+++ src/dk/easv/bll/bot/SeymourBot.java
@@ -1,14 +1,17 @@
 package dk.easv.bll.bot;
 
 import dk.easv.bll.field.IField;
+import dk.easv.bll.game.GameState;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class SeymourBot implements IBot{
-    private static final String BOTNAME = "Seymour-Bot";
+    final int moveTimeMs = 1000;
+    private static final String BOTNAME = "Seymour-Bots";
     protected int[][] preferredMoves = {
             {0, 1}, {2, 1}, {1, 0}, {1, 2}, //Outer Middles ordered across
             {0, 0}, {2, 2}, {0, 2}, {2, 0},  //Corners ordered across
@@ -27,8 +30,10 @@ public class SeymourBot implements IBot{
         move = null;
 
         List<IMove> moves = state.getField().getAvailableMoves();
-        System.out.println(moves);
-        move = preferable(state);
+
+        move = calculateWinningMove(state, moveTimeMs);
+
+        //move = preferable(state);
 
         if (move == null) return random(moves);
 
@@ -66,5 +71,279 @@ public class SeymourBot implements IBot{
         }
 
         return null;
+    }
+
+    private SeymourBot.GameSimulator createSimulator(IGameState state) {
+        SeymourBot.GameSimulator simulator = new SeymourBot.GameSimulator(new GameState());
+        simulator.setGameOver(SeymourBot.GameOverState.Active);
+        simulator.setCurrentPlayer(state.getMoveNumber() % 2);
+        simulator.getCurrentState().setRoundNumber(state.getRoundNumber());
+        simulator.getCurrentState().setMoveNumber(state.getMoveNumber());
+        simulator.getCurrentState().getField().setBoard(state.getField().getBoard());
+        simulator.getCurrentState().getField().setMacroboard(state.getField().getMacroboard());
+        return simulator;
+    }
+
+    // Plays single games until it wins and returns the first move for that. If iterations reached with no clear win, just return random valid move
+    private IMove calculateWinningMove(IGameState state, int maxTimeMs){
+        long time = System.currentTimeMillis();
+        Random rand = new Random();
+        int count = 0;
+        while (System.currentTimeMillis() < time + maxTimeMs) { // check how much time has passed, stop if over maxTimeMs
+            SeymourBot.GameSimulator simulator = createSimulator(state);
+            IGameState gs = simulator.getCurrentState();
+            List<IMove> moves = gs.getField().getAvailableMoves();
+            IMove randomMovePlayer = moves.get(rand.nextInt(moves.size()));
+            IMove winnerMove = randomMovePlayer;
+
+            while (simulator.getGameOver()== SeymourBot.GameOverState.Active){ // Game not ended
+                simulator.updateGame(randomMovePlayer);
+
+                // Opponent plays randomly
+                if (simulator.getGameOver()== SeymourBot.GameOverState.Active){ // game still going
+                    moves = gs.getField().getAvailableMoves();
+                    IMove randomMoveOpponent = moves.get(rand.nextInt(moves.size()));
+                    simulator.updateGame(randomMoveOpponent);
+                }
+                if (simulator.getGameOver()== SeymourBot.GameOverState.Active){ // game still going
+                    moves = gs.getField().getAvailableMoves();
+                    randomMovePlayer = moves.get(rand.nextInt(moves.size()));
+                }
+            }
+
+            if (simulator.getGameOver()== SeymourBot.GameOverState.Win){
+                //System.out.println("Found a win, :)");
+                return winnerMove; // Hint you could maybe save multiple games and pick the best? Now it just returns at a possible victory
+            }
+            count++;
+        }
+        //System.out.println("Did not win, just doing random :¨(");
+        List<IMove> moves = state.getField().getAvailableMoves();
+        IMove randomMovePlayer = moves.get(rand.nextInt(moves.size()));
+        return randomMovePlayer; // just play randomly if solution not found
+    }
+
+    /*
+        The code below is a simulator for simulation of gameplay. This is needed for AI.
+
+        It is put here to make the Bot independent of the GameManager and its subclasses/enums
+
+        Now this class is only dependent on a few interfaces: IMove, IField, and IGameState
+
+        You could say it is self-contained. The drawback is that if the game rules change, the simulator must be
+        changed accordingly, making the code redundant.
+
+     */
+
+    public enum GameOverState {
+        Active,
+        Win,
+        Tie
+    }
+
+    public class Move implements IMove {
+        int x = 0;
+        int y = 0;
+
+        public Move(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int getX() {
+            return x;
+        }
+
+        @Override
+        public int getY() {
+            return y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            SeymourBot.Move move = (SeymourBot.Move) o;
+            return x == move.x && y == move.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    class GameSimulator {
+        private final IGameState currentState;
+        private int currentPlayer = 0; //player0 == 0 && player1 == 1
+        private volatile SeymourBot.GameOverState gameOver = SeymourBot.GameOverState.Active;
+
+        public void setGameOver(SeymourBot.GameOverState state) {
+            gameOver = state;
+        }
+
+        public SeymourBot.GameOverState getGameOver() {
+            return gameOver;
+        }
+
+        public void setCurrentPlayer(int player) {
+            currentPlayer = player;
+        }
+
+        public IGameState getCurrentState() {
+            return currentState;
+        }
+
+        public GameSimulator(IGameState currentState) {
+            this.currentState = currentState;
+        }
+
+        public Boolean updateGame(IMove move) {
+            if (!verifyMoveLegality(move))
+                return false;
+
+            updateBoard(move);
+            currentPlayer = (currentPlayer + 1) % 2;
+
+            return true;
+        }
+
+        private Boolean verifyMoveLegality(IMove move) {
+            IField field = currentState.getField();
+            boolean isValid = field.isInActiveMicroboard(move.getX(), move.getY());
+
+            if (isValid && (move.getX() < 0 || 9 <= move.getX())) isValid = false;
+            if (isValid && (move.getY() < 0 || 9 <= move.getY())) isValid = false;
+
+            if (isValid && !field.getBoard()[move.getX()][move.getY()].equals(IField.EMPTY_FIELD))
+                isValid = false;
+
+            return isValid;
+        }
+
+        private void updateBoard(IMove move) {
+            String[][] board = currentState.getField().getBoard();
+            board[move.getX()][move.getY()] = currentPlayer + "";
+            currentState.setMoveNumber(currentState.getMoveNumber() + 1);
+            if (currentState.getMoveNumber() % 2 == 0) {
+                currentState.setRoundNumber(currentState.getRoundNumber() + 1);
+            }
+            checkAndUpdateIfWin(move);
+            updateMacroboard(move);
+
+        }
+
+        private void checkAndUpdateIfWin(IMove move) {
+            String[][] macroBoard = currentState.getField().getMacroboard();
+            int macroX = move.getX() / 3;
+            int macroY = move.getY() / 3;
+
+            if (macroBoard[macroX][macroY].equals(IField.EMPTY_FIELD) ||
+                    macroBoard[macroX][macroY].equals(IField.AVAILABLE_FIELD)) {
+
+                String[][] board = getCurrentState().getField().getBoard();
+
+                if (isWin(board, move, "" + currentPlayer))
+                    macroBoard[macroX][macroY] = currentPlayer + "";
+                else if (isTie(board, move))
+                    macroBoard[macroX][macroY] = "TIE";
+
+                //Check macro win
+                if (isWin(macroBoard, new SeymourBot.Move(macroX, macroY), "" + currentPlayer))
+                    gameOver = SeymourBot.GameOverState.Win;
+                else if (isTie(macroBoard, new SeymourBot.Move(macroX, macroY)))
+                    gameOver = SeymourBot.GameOverState.Tie;
+            }
+
+        }
+
+        private boolean isTie(String[][] board, IMove move) {
+            int localX = move.getX() % 3;
+            int localY = move.getY() % 3;
+            int startX = move.getX() - (localX);
+            int startY = move.getY() - (localY);
+
+            for (int i = startX; i < startX + 3; i++) {
+                for (int k = startY; k < startY + 3; k++) {
+                    if (board[i][k].equals(IField.AVAILABLE_FIELD) ||
+                            board[i][k].equals(IField.EMPTY_FIELD))
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        public boolean isWin(String[][] board, IMove move, String currentPlayer) {
+            int localX = move.getX() % 3;
+            int localY = move.getY() % 3;
+            int startX = move.getX() - (localX);
+            int startY = move.getY() - (localY);
+
+            //check col
+            for (int i = startY; i < startY + 3; i++) {
+                if (!board[move.getX()][i].equals(currentPlayer))
+                    break;
+                if (i == startY + 3 - 1) return true;
+            }
+
+            //check row
+            for (int i = startX; i < startX + 3; i++) {
+                if (!board[i][move.getY()].equals(currentPlayer))
+                    break;
+                if (i == startX + 3 - 1) return true;
+            }
+
+            //check diagonal
+            if (localX == localY) {
+                //we're on a diagonal
+                int y = startY;
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][y++].equals(currentPlayer))
+                        break;
+                    if (i == startX + 3 - 1) return true;
+                }
+            }
+
+            //check anti diagonal
+            if (localX + localY == 3 - 1) {
+                int less = 0;
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][(startY + 2) - less++].equals(currentPlayer))
+                        break;
+                    if (i == startX + 3 - 1) return true;
+                }
+            }
+            return false;
+        }
+
+        private void updateMacroboard(IMove move) {
+            String[][] macroBoard = currentState.getField().getMacroboard();
+            for (int i = 0; i < macroBoard.length; i++)
+                for (int k = 0; k < macroBoard[i].length; k++) {
+                    if (macroBoard[i][k].equals(IField.AVAILABLE_FIELD))
+                        macroBoard[i][k] = IField.EMPTY_FIELD;
+                }
+
+            int xTrans = move.getX() % 3;
+            int yTrans = move.getY() % 3;
+
+            if (macroBoard[xTrans][yTrans].equals(IField.EMPTY_FIELD))
+                macroBoard[xTrans][yTrans] = IField.AVAILABLE_FIELD;
+            else {
+                // Field is already won, set all fields not won to avail.
+                for (int i = 0; i < macroBoard.length; i++)
+                    for (int k = 0; k < macroBoard[i].length; k++) {
+                        if (macroBoard[i][k].equals(IField.EMPTY_FIELD))
+                            macroBoard[i][k] = IField.AVAILABLE_FIELD;
+                    }
+            }
+        }
     }
 }
